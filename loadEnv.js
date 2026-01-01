70d17b31c28ee97b0acd5b2d503eba2c79db7075
yanliveradio-design <yanliveradio@gmail.com>
2026-01-01 22:23:46 +0700

    Delete loadEnv.js

--- loadEnv.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-      
-      const envConfig = JSON.parse(fs.readFileSync('env.json', 'utf8'));
-      
-      Object.keys(envConfig).forEach(key => {
-        process.env[key] = envConfig[key];
-      });
